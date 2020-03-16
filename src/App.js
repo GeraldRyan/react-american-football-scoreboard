1b4c50dddf8ef29212488f2d8640198f2001fd39
Webbuildermn <gerald.ryan40@yahoo.com>
2020-03-16 17:53:19 -0500

    Getting advanced stretch functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@
 import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import quarter from "./BottomRow"
+import setQuarter from "./BottomRow"
+import upQuarter from "./BottomRow"
+
 
 function App()
 {
@@ -9,7 +13,10 @@ function App()
 
   const [homeScore, setHomeScore] = useState(0)
   const [awayScore, setAwayScore] = useState(0)
-  // const [quarter, setQuarter] = useState(0)
+  const [quarter, setQuarter] = useState(1)
+  const [down, setDown] = useState(1)
+  const [ballOn, setBallOn] = useState(99)
+  
 
 
   function clickAction(team, amount)
@@ -20,12 +27,56 @@ function App()
 
     else if (team === 'away')
 
-    { (setAwayScore(awayScore + amount))}
+    { (setAwayScore(awayScore + amount)) }
   }
 
-// function upQuarter(){
-//   setQuarter(quarter + 1)
-// }
+  function increment(what)
+  {
+    switch (what)
+    {
+      case 'down':
+        if (down % 4 == 0) { setDown(1) }
+        else { setDown(down + 1) }
+        break
+
+      case 'quarter':
+        if (quarter % 4 == 0) { setQuarter(1) }
+        else { setQuarter(quarter + 1) }
+        break
+    }
+  }
+
+
+function yardsRan(){
+  const x = Math.floor(Math.random()*20)
+  if (x> ballOn){
+    setBallOn(0)
+    console.log("Touchdown!")
+  }
+  else{
+    setBallOn(ballOn - x)
+  }
+  if (ballOn <1){
+    alert("Touchdown!!!")
+  }
+}
+
+
+
+
+
+  // REFACTORED
+  // function upQuarter()
+  // {
+  //   if (quarter % 4 == 0) { setQuarter(1) }
+  //   else { setQuarter(quarter + 1) }
+  // }
+
+  // function upDown()
+  // {
+  //   if (down % 4 == 0) { setDown(1) }
+  //   else { setDown(down + 1) }
+  // }
 
 
 
@@ -46,7 +97,7 @@ function App()
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow down={down} quarter={quarter} ballOn={ballOn} />
       </section>
       <section className="buttons">
         <div className="homeButtons">
@@ -55,10 +106,14 @@ function App()
           <button className="homeButtons__fieldGoal" onClick={() => clickAction('home', 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick= {()=> clickAction('away', 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal"onClick= {()=> clickAction('away', 3)}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => clickAction('away', 7)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => clickAction('away', 3)}>Away Field Goal</button>
         </div>
-        {/* <button className="quarterup" onClick= {()=> upQuarter()}>Increment quarter</button> */}
+        <button className="downup" onClick={() => increment('down')}>Next Down</button>
+        <button className="quarterup" onClick={() => increment('quarter')}>Increment quarter</button>
+        <button className="yards" onClick={() => yardsRan()}>Run a play</button>
+
+
       </section>
     </div>
   );
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,27 +1,12 @@
 import React, {useState} from "react";
 import "./App.css";
 
-
-
-
-
-// const [quarter, setQuarter] = useState(0)
-
-
-// function upQuarter(){
-//   setQuarter(quarter + 1)
-// }
-
-
-
-
-
-const BottomRow = () => {
+const BottomRow = (props) => {
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{props.down}</div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
@@ -29,14 +14,15 @@ const BottomRow = () => {
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <div className="ballOn__value">{props.ballOn}</div>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{props.quarter}</div>
       </div>
     </div>
   );
 };
+
 
 export default BottomRow;
