16c40176b3b4e6695202def0d821471dd1a111ab
Webbuildermn <gerald.ryan40@yahoo.com>
2020-03-16 21:14:13 -0500

    Making my game better

--- src/App.js
+++ src/App.js
@@ -11,24 +11,58 @@ function App()
   const [awayScore, setAwayScore] = useState(0)
   const [quarter, setQuarter] = useState(1)
   const [down, setDown] = useState(1)
-  const [ballOn, setBallOn] = useState(99)
+  const [ballOn, setBallOn] = useState(90)
   const [possessor, setPossessor] = useState('home')
   const [numPlays, setNumPlays] = useState(0)
-  const [totalSecondsInQuarter, setTotalSecondsInQuarter] = useState(15*60)
+  const [totalSecondsInQuarter, setTotalSecondsInQuarter] = useState(15 * 60)
   const [minutesInQuarter, setMinutesInQuarter] = useState(15)
   const [secondsInQuarter, setSecondsInQuarter] = useState(0)
-
+  const [togo, setTogo] = useState(10)
 
 
   // if (tSeconds ===0){
   //   setTimer(`${tMinutes}:00`)
   // }
 
-  function changeTime(){
-    const secondsPassed = Math.floor(Math.random()*20)
+
+  function kickFG()
+  {
+    if (down === 4 && ballOn < 45)
+    {
+      if (possessor == 'home')
+      {
+        setHomeScore(homeScore + 3)
+      }
+      else (setAwayScore(awayScore + 3))
+      console.log("field goal")
+    }
+    turnover()
+
+  }
+
+  function turnover()
+  {
+    if (possessor === 'home')
+    {
+      setPossessor("away")
+    }
+    else
+    {
+      setPossessor('home')
+    }
+    setDown(1)
+    if (ballOn > 10)
+    {
+      setTogo(10)
+    }
+    else { setTogo(ballOn) }
+  }
+  function changeTime()
+  {
+    const secondsPassed = Math.floor(Math.random() * 20)
     setTotalSecondsInQuarter(totalSecondsInQuarter - secondsPassed)
-    setMinutesInQuarter(Math.floor(totalSecondsInQuarter/60))
-    setSecondsInQuarter(totalSecondsInQuarter - minutesInQuarter*60)
+    setMinutesInQuarter(Math.floor(totalSecondsInQuarter / 60))
+    setSecondsInQuarter(totalSecondsInQuarter - minutesInQuarter * 60)
   }
 
   function clickAction(team, amount)
@@ -64,30 +98,98 @@ function App()
     }
   }
 
-function newGame(){
-  setQuarter(1)
-  setAwayScore(0)
-  setHomeScore(0)
-  setDown(1)
-  setTotalSecondsInQuarter(15*60)
-}
+  function firstDown(){
+    setTogo(10)
+    setDown(1)
+  }
 
+  function touchdown(team)
+  {
+    if (team == 'home')
+    {
+      setHomeScore(homeScore + 7)
+    }
+    else { setAwayScore(awayScore + 7) }
+    turnover()
+  }
+
+  function newGame()
+  {
+    setQuarter(1)
+    setAwayScore(0)
+    setHomeScore(0)
+    setDown(1)
+    setTotalSecondsInQuarter(15 * 60)
+    setMinutesInQuarter(15)
+    setSecondsInQuarter(0)
+    setBallOn(90)
+    setTogo(10)
+  }
+
+  function throwPass()
+  {
+
+    setNumPlays(numPlays + 1)
+    changeTime()
+    let yardsReceived = 0
+    const r = Math.random()
+    if (r < .15) { yardsReceived = Math.floor(Math.random() * -5) }
+    else if (r < .33)
+    { // 0-3 yards
+      yardsReceived = Math.floor(Math.random() * 3)
+    }
+    else if (r < .66)
+    { yardsReceived = Math.floor(Math.random() * 4) + 3 } // 3-7
+    else { yardsReceived = Math.floor(Math.random() * 60) }
+
+    if (yardsReceived > ballOn)
+    {
+      console.log('touchdown')
+      touchdown()
+    }
+    else if (yardsReceived < togo && down === 4)
+    {
+      setBallOn(100-(ballOn - yardsReceived))
+      turnover()
+    }
+    else if (yardsReceived > togo)
+    {
+      firstDown()
+      setTogo(togo-yardsReceived)
+      setBallOn(ballOn-yardsReceived)
+    }
+    setBallOn(ballOn - yardsReceived)
+    setDown(down + 1)
+  }
 
   function runPlay()
   {
     setNumPlays(numPlays + 1)
-    console.log(numPlays)
     changeTime()
-    if (totalSecondsInQuarter <1 && quarter ===4){
-      alert(`We have ourselves a winner! Congratulations ${possessor} team, by a margin of ${Math.abs(homeScore-setAwayScore)}. Let's play again!`)
+    if (totalSecondsInQuarter < 1 && quarter === 4)
+    {
+      alert(`We have ourselves a winner! Congratulations ${possessor} team, by a margin of ${Math.abs(homeScore - setAwayScore)}. Let's play again!`)
       newGame()
     }
-    else if (totalSecondsInQuarter <1){
-      setQuarter(quarter +1)
-      setTotalSecondsInQuarter(15*60)
+    else if (totalSecondsInQuarter < 1)
+    {
+      setQuarter(quarter + 1)
+      setTotalSecondsInQuarter(15 * 60)
     }
-
-    const yardsRan = Math.floor(Math.random() * 30)  // random between 0-30
+    const c = Math.random()
+    let yardsRan = Math.floor(Math.random() * 100)  // random between 0-30
+    if (c < .33)
+    {
+      yardsRan = 0
+    }
+    else if (c < .6)
+    { yardsRan = Math.floor(yardsRan * .1) }
+    else if (c < .75)
+    {
+      yardsRan = Math.floor(yardsRan * .25)
+    }
+    else if (c < .96) { yardsRan = Math.floor(yardsRan * .5) }
+    console.log(yardsRan)
 
     if (yardsRan > ballOn)  // if more than enough to score
     {
@@ -105,19 +207,21 @@ function newGame(){
       }
       setDown(1)
     }
+    else if (yardsRan >= togo)
+    {
+      setTogo(10)
+      setDown(1)
+    }
     else
     {
       if (down === 4)
       {
         setBallOn(75)
-        if (possessor === 'home') { setPossessor('away') } else
-        {
-          setPossessor('home')
-        }
-        setDown(1)
+        turnover()
       }
       else
       {
+        setTogo(togo - yardsRan)
         setBallOn(ballOn - yardsRan)
         increment('down')
       }
@@ -162,7 +266,7 @@ function newGame(){
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow down={down} quarter={quarter} ballOn={ballOn} possessor={possessor} />
+        <BottomRow down={down} quarter={quarter} ballOn={ballOn} possessor={possessor} togo={togo} />
       </section>
       <section className="buttons">
         <div className="homeButtons">
@@ -177,6 +281,7 @@ function newGame(){
         <button className="downup" onClick={() => increment('down')}>Next Down</button>
         <button className="quarterup" onClick={() => increment('quarter')}>Increment quarter</button>
         <button className="yards" onClick={() => runPlay()}>Run a play</button>
+        <button className="yards" onClick={() => throwPass()}>Throw a pass</button>
         <button className="yards" onClick={() => newGame()}>Start Over</button>
 
 
